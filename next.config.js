15c947b8f1030424dca122eb7b9fb4b628ca9749
neeraj15y86 <neeraj15y86@gmail.com>
2025-07-07 21:23:56 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,8 +1,50 @@
-// next.config.js
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // any Next.js-specific settings here
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // any Next.js-specific settings here
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Applies to all routes
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload', // Enforce HTTPS
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; img-src * data:; script-src 'self'; style-src 'self' 'unsafe-inline'",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
