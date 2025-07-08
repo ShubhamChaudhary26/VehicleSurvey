d1536ee451c10d8f7492b87dbf5b2c3a262bec4b
neeraj15y86 <neeraj15y86@gmail.com>
2025-07-08 08:40:13 +0530

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
 import VehicleSurveyForm from '@/component/VehicleSurveyForm'
 import React from 'react'
 
+export const metadata = {
+  title: 'MintSurvey - Vehicle Ownership Survey',
+  description: 'Participate in MintSurvey Vehicle Ownership Survey to share your experience and help shape the future of mobility.',
+  keywords: ['vehicle', 'survey', 'form', 'feedback'],
+  openGraph: {
+  title: 'MintSurvey - Vehicle Ownership Survey',
+  description: 'Participate in MintSurvey Vehicle Ownership Survey to share your experience and help shape the future of mobility.',
+  url: 'https://www.micollectionapp.com/',
+  type: 'website',
+  images: [
+    {
+      url: 'https://www.micollectionapp.com/MintSurveyLogo.png', // ✅ Full URL to image
+      width: 1200,
+      height: 630,
+      alt: 'MintSurvey - Vehicle Ownership Survey',
+    },
+  ],
+},
+};
+
 const page = () => {
   return (
     <div><VehicleSurveyForm/></div>
